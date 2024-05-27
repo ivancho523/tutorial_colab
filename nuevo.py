8a060d15759f324bd41ae441c940351a48104979
Ivan Zapata <zerox523@gmail.com>
2024-05-26 21:35:19 -0300

    creación archivo de gitignore y modificación archivo python

--- nuevo.py
+++ nuevo.py
@@ -3,3 +3,10 @@ import pandas as pd
 
 print("hola desde el escirotio")
 
+# Definir la matriz de 2x2
+matriz_2x2 = np.array([[1, 2], [3, 4]])
+
+# Calcular el promedio de la matriz
+promedio_matriz = np.mean(matriz_2x2)
+
+print("El promedio de la matriz de 2x2 es:", promedio_matriz)
